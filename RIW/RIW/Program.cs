a1505fb68c8caaff5302f8baefaa55f5aa82ea43
SpentTime <justin.sepulveda@hotmail.com>
2015-06-29 11:24:23 -0700

    RIW
    Attempt to reinvent the wheel and write my own console text editor

---
+++ RIW/RIW/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RIW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void doWork()
+        {
+            ScreenManipulator screenInfo = new ScreenManipulator();
+            do
+            {
+                screenInfo.getInput(Console.ReadKey(true));
+                Console.Clear();
+                foreach (var i in screenInfo.ScreenText)
+                    Console.WriteLine(i);
+                Console.CursorLeft = screenInfo.LeftPosition;
+                Console.CursorTop = screenInfo.TopPosition;
+
+            } while (true);
+        }
+    }
+}
---
+++ RIW/RIW/ScreenManipulator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RIW
+{
+    class ScreenManipulator
+    {
+        private List<string> _screenText;
+        private int _topPosition, _leftPosition;
+
+        public List<string> ScreenText { get { return _screenText; } }
+
+        public int TopPosition
+        {
+            get { return this._topPosition; }
+            private set
+            {
+                if (value < 0) 
+                    this._topPosition = value; 
+                else if (value < this._screenText.Count())
+                {
+                    this._topPosition = value;
+                    this.LeftPosition = this._leftPosition;
+                }
+            }
+        }
+        
+        public int LeftPosition
+        {
+            get { return this._leftPosition; }
+            private set
+            {
+                if (value < 0) 
+                    this._leftPosition = value;
+                else if (value < this._screenText[TopPosition].Count())
+                    this._leftPosition = value;
+                else if (value >= this._screenText[TopPosition].Count())
+                    this._leftPosition = this._screenText[TopPosition].Count();
+            }
+        }
+
+        private void AddToScreenText(char c)
+        {
+            if (c > 31 && c < 127)  // if c is a printable character
+                InsertChar(c);
+            else if (c == '\n')
+                InsertNewLine();
+        }
+
+        private void InsertChar(char c)
+        {
+            if (this.LeftPosition < this._screenText[this.TopPosition].Count()) // if cursor is not at end of line, insert character
+                this._screenText[this.TopPosition] = this._screenText[this.TopPosition].Insert(this.LeftPosition, c.ToString());
+            else
+            {
+                this._screenText[this.TopPosition] += c;
+                LeftPosition++;
+            }
+            LeftPosition++;
+        }
+
+        private void InsertNewLine()
+        {
+            this._screenText.Insert(TopPosition + 1, this._screenText[TopPosition].Substring(LeftPosition));    
+            this._screenText[TopPosition] = this._screenText[TopPosition].Substring(0, LeftPosition);
+            this.TopPosition++;
+            this.LeftPosition = this._screenText[TopPosition].Count();
+        }
+
+        public ScreenManipulator()
+        {
+            this._screenText = new List<string>() { "" };
+            this._topPosition = 0;
+            this._leftPosition = 0;
+        }
+
+        public void getInput(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    this.TopPosition--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    this.TopPosition++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    this.LeftPosition--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    this.LeftPosition++;
+                    break;
+                case ConsoleKey.Enter:
+                    this.AddToScreenText('\n');
+                    break;
+                default:
+                    this.AddToScreenText(keyInfo.KeyChar);
+                    break;
+            }
+        }
+    }
+}
